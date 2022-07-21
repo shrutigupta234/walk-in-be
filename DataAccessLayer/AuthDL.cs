1fb6b17dcedd9cbf17fb074e06dbca368c6dc8c2
Shruti <shruti.gupta@zeuslearning.com>
2022-07-21 20:48:04 +0530

    registration

--- DataAccessLayer/AuthDL.cs
+++ DataAccessLayer/AuthDL.cs
@@ -119,7 +119,7 @@ namespace walk_in_api.DataAccessLayer
                     await _mySqlConnection.OpenAsync();
                 }
 
-                string SqlQuery = @"INSERT INTO walk.user (username, password) values(@username, @password);";
+                string SqlQuery = @"INSERT INTO walk.user (username, password, fname,lname,email,phone,portfolio_url,is_email_update,aggregate,year_of_passing,qualification,stream ,college ,college_city, applicant_type ,yoe,current_ctc,expected_ctc,is_notice_period, end_notice_date ,notice_duration,is_prev_test , prev_role_applied ) values(@username, @password,@fname,@lname,@email,@phone,@portfolio_url,@is_email_update,@aggregate,@year_of_passing,@qualification,@stream ,@college ,@college_city,@applicant_type ,@yoe,@current_ctc,@expected_ctc,@is_notice_period,@end_notice_date ,@notice_duration,@is_prev_test ,@prev_role_applied );";
 
                 using(MySqlCommand sqlCommand = new MySqlCommand(SqlQuery, _mySqlConnection))
                 {
@@ -127,6 +127,27 @@ namespace walk_in_api.DataAccessLayer
                     sqlCommand.CommandTimeout = 180;
                     sqlCommand.Parameters.AddWithValue("@username",request.username);
                     sqlCommand.Parameters.AddWithValue("@password",encoder.Encode(request.password));
+                    sqlCommand.Parameters.AddWithValue("@fname",request.fname);
+                    sqlCommand.Parameters.AddWithValue("@lname",request.lname);
+                    sqlCommand.Parameters.AddWithValue("@email",request.email);
+                    sqlCommand.Parameters.AddWithValue("@phone",request.phone);
+                    sqlCommand.Parameters.AddWithValue("@portfolio_url",request.portfolio_url);
+                    sqlCommand.Parameters.AddWithValue("@is_email_update",request.is_email_update);
+                    sqlCommand.Parameters.AddWithValue("@aggregate",request.aggregate);
+                    sqlCommand.Parameters.AddWithValue("@year_of_passing",request.year_of_passing);
+                    sqlCommand.Parameters.AddWithValue("@qualification",request.qualification);
+                    sqlCommand.Parameters.AddWithValue("@stream",request.stream);
+                    sqlCommand.Parameters.AddWithValue("@college",request.college);
+                    sqlCommand.Parameters.AddWithValue("@college_city",request.college_city);
+                    sqlCommand.Parameters.AddWithValue("@applicant_type",request.applicant_type);
+                    sqlCommand.Parameters.AddWithValue("@yoe",request.yoe);
+                    sqlCommand.Parameters.AddWithValue("@current_ctc",request.current_ctc);
+                    sqlCommand.Parameters.AddWithValue("@expected_ctc",request.expected_ctc);
+                    sqlCommand.Parameters.AddWithValue("@is_notice_period",request.is_notice_period);
+                    sqlCommand.Parameters.AddWithValue("@end_notice_date",request.end_notice_date);
+                    sqlCommand.Parameters.AddWithValue("@notice_duration",request.notice_duration);
+                    sqlCommand.Parameters.AddWithValue("@is_prev_test",request.is_prev_test);
+                    sqlCommand.Parameters.AddWithValue("@prev_role_applied", request.prev_role_applied);
                     int Status = await sqlCommand.ExecuteNonQueryAsync();
 
                     if(Status <= 0){
--- Model/SignUp.cs
+++ Model/SignUp.cs
@@ -19,6 +19,68 @@ namespace walk_in_api.Model
         [Required]
         public string? confirmPassword {get; set;}
 
+        [Required]
+        public string? fname {get; set;}
+
+        [Required]
+        public string? lname {get; set;}
+
+        [Required]
+        public string? email {get; set;}
+
+        [Required]
+        public string? phone {get; set;}
+
+        public string? portfolio_url {get; set;}
+
+        [Required]
+        public bool? is_email_update {get; set;}
+
+        [Required]
+        public decimal? aggregate {get; set;}
+
+        [Required]
+        public int? year_of_passing {get; set;}
+
+        [Required]
+        public string? qualification {get; set;}
+
+        [Required]
+        public string? stream {get; set;}
+
+        [Required]
+        public string? college {get; set;}
+
+        [Required]
+        public string? college_city {get; set;}
+
+        [Required]
+        public string? applicant_type {get; set;}
+
+        [Required]
+        public int? yoe {get; set;}
+
+        [Required]
+        public int? current_ctc {get; set;}
+
+        [Required]
+        public int? expected_ctc {get; set;}
+
+        [Required]
+        public bool? is_notice_period {get; set;}
+
+        [Required]
+        public DateTime? end_notice_date {get; set;}
+
+        [Required]
+        public int? notice_duration {get; set;}
+
+        [Required]
+        public bool? is_prev_test {get; set;}
+
+        public string? prev_role_applied {get; set;}
+
+
 
 
     }
@@ -31,3 +93,9 @@ namespace walk_in_api.Model
 
     }
 }
+
+
+
+
+       
+        //  @fname,@lname,@email,@phone,@portfolio,@is_email_update,@aggregate,@year_of_passing,@qualification,@stream ,@college ,@college_city,@ applicant_type ,@yoe,@current_ctc,@expected_ctc,@is_notice_period,@ end_notice_date ,@notice_duration,@is_prev_test ,@prev_role_applied 
